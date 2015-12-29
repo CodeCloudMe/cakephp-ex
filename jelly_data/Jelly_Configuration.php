a8b563fb815cd6b94a4b9da35083e004e6321411
Mike <m@thecoded.org>
2015-12-29 22:02:38 +0900

    added ability

--- jelly_data/Jelly_Configuration.php
+++ jelly_data/Jelly_Configuration.php
@@ -16,7 +16,7 @@ array(
 		"Username" => getenv('OPENSHIFT_MYSQL_DB_USERNAME'),
 				
 		// MySQL Database Password
-		"Password" => getenv('PASSWORD'),
+		"Password" => getenv('OPENSHIFT_MYSQL_DB_PASSWORD'),
 				
 		// MySQL Table Prefix
 		"Table_Prefix" => ""
