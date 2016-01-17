e21eb3ebd27493a18f92af4bbd3cf484fab35d64
Mike <m@thecoded.org>
2016-01-18 01:13:25 +0900

    jelly bitch

--- jelly/php/helpers/Get_Environment_Variables.php
+++ jelly/php/helpers/Get_Environment_Variables.php
@@ -12,7 +12,7 @@ function &Get_Environment_Variables()
 				"Database_Name" => getenv("DATABASE_NAME"),
 				
 				// Database Host Name
-				"Host_Name" => "172.17.0.3",
+				"Host_Name" => "localhost",
 				
 				// Database Username
 				"Username" => getenv('DATABASE_USER'),
