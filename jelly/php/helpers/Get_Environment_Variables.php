68de5d45cc3a44fbc232cc83a42451ad1ea68e10
Mike <m@thecoded.org>
2016-01-19 10:40:29 +0900

    new cred for mysql

--- jelly/php/helpers/Get_Environment_Variables.php
+++ jelly/php/helpers/Get_Environment_Variables.php
@@ -12,7 +12,7 @@ function &Get_Environment_Variables()
 				"Database_Name" => getenv("DATABASE_NAME"),
 				
 				// Database Host Name
-				"Host_Name" => "172.17.0.6",	
+				"Host_Name" => "172.17.0.2",	
 				
 				// Database Username
 				"Username" => getenv('DATABASE_USER'),
