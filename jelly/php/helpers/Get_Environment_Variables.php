03407cb1f93fb615977b90b3367bf44bf2730fb2
Mike <m@thecoded.org>
2016-01-19 20:24:50 +0900

    save

--- jelly/php/helpers/Get_Environment_Variables.php
+++ jelly/php/helpers/Get_Environment_Variables.php
@@ -12,7 +12,7 @@ function &Get_Environment_Variables()
 				"Database_Name" => "sampledb",
 				
 				// Database Host Name
-				"Host_Name" => "172.17.0.8",	
+				"Host_Name" => "172.17.0.5",	
 				
 				// Database Username
 				"Username" => 'userLHK',
